1956d164f040ae77acc2bb742e703c0c28017189
alexinea <forerunner@outlook.com>
2019-04-29 10:04:16 +0800

    Add a sample project for ASP.NET Core MVC

---
+++ samples/Cosmos.Business.Samples/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Cosmos.Business.Extensions.FileTypeSniffers;
+using Microsoft.AspNetCore.Mvc;
+using Cosmos.Business.Samples.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Cosmos.Business.Samples.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly IFileTypeSniffer _sniffer;
+
+        public HomeController(IHostingEnvironment env, IFileTypeSniffer sniffer)
+        {
+            _env = env;
+            _sniffer = sniffer;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sniffer()
+        {
+            var path = _env.WebRootFileProvider.GetFileInfo("img/sniffer.jpg").PhysicalPath;
+            byte[] array = new byte[20];
+            using (var file = System.IO.File.Open(path, FileMode.Open))
+            {
+                file.Read(array, 0, array.Length);
+            }
+
+            var results = _sniffer.Match(array, true);
+            var statistics = _sniffer.GetMetadataStatistics();
+            var content = $@"
+statistics: total={statistics.Total}
+result: {results.ToListString()}
+";
+            return Content(content);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
