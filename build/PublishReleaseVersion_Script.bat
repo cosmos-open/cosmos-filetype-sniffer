a45d7086b0776388517a559b40ede8f35a6ec871
alexinea <forerunner@outlook.com>
2020-09-09 16:41:22 +0800

    Update solution files.

--- build/PublishReleaseVersion_Script.bat
+++ build/PublishReleaseVersion_Script.bat
@@ -25,10 +25,6 @@ echo.
 ::core
 dotnet pack src/Cosmos.Extensions.FileTypeSniffer/Cosmos.Extensions.FileTypeSniffer._build.csproj                                         -c Release -o nuget_packages --no-restore
 
-::extensions for dependency
-dotnet pack src/Cosmos.Extensions.FileTypeSniffer.Extensions.Autofac/Cosmos.Extensions.FileTypeSniffer.Extensions.Autofac._build.csproj   -c Release -o nuget_packages --no-restore
-dotnet pack src/Cosmos.Extensions.FileTypeSniffer.Extensions.DependencyInjection/Cosmos.Extensions.FileTypeSniffer.Extensions.DependencyInjection._build.csproj -c Release -o nuget_packages --no-restore
-
 ::extensions for library
 dotnet pack src/Cosmos.Extensions.FileTypeSniffer.Library.Development/Cosmos.Extensions.FileTypeSniffer.Library.Development._build.csproj -c Release -o nuget_packages --no-restore
 dotnet pack src/Cosmos.Extensions.FileTypeSniffer.Library.Image/Cosmos.Extensions.FileTypeSniffer.Library.Image._build.csproj             -c Release -o nuget_packages --no-restore
@@ -36,6 +32,27 @@ dotnet pack src/Cosmos.Extensions.FileTypeSniffer.Library.Media/Cosmos.Extension
 dotnet pack src/Cosmos.Extensions.FileTypeSniffer.Library.Office/Cosmos.Extensions.FileTypeSniffer.Library.Office._build.csproj           -c Release -o nuget_packages --no-restore
 dotnet pack src/Cosmos.Extensions.FileTypeSniffer.Library.Science/Cosmos.Extensions.FileTypeSniffer.Library.Science._build.csproj         -c Release -o nuget_packages --no-restore
 dotnet pack src/Cosmos.Extensions.FileTypeSniffer.Library.Zip/Cosmos.Extensions.FileTypeSniffer.Library.Zip._build.csproj                 -c Release -o nuget_packages --no-restore
+
+echo =======================================================================
+echo Cosmos.Extensions.FileTypeSniffer EXTRA
+echo =======================================================================
+
+::start to build, package and push solo/extra projects
+cd extra/AspectCore
+dotnet build FileTypeSniffer-Extra-AspectCore.sln
+cd ../..
+
+cd extra/Autofac
+dotnet build FileTypeSniffer-Extra-Autofac.sln
+cd ../..
+
+cd extra/DI
+dotnet build FileTypeSniffer-Extra-DI.sln
+cd ../..
+
+dotnet pack extra/AspectCore/src/Extra/Extra.csproj    -c Release -o nuget_packages --no-restore
+dotnet pack extra/Autofac/src/Extra/Extra.csproj       -c Release -o nuget_packages --no-restore
+dotnet pack extra/DI/src/Extra/Extra.csproj            -c Release -o nuget_packages --no-restore
 
 for /R "nuget_packages" %%s in (*symbols.nupkg) do (
     del "%%s"
