75208732dfe804f79429483abb9b2cef844fdf74
alexinea <forerunner@outlook.com>
2021-07-14 12:35:48 +0800

    Update solution and project files.

---
+++ src/Cosmos.Extensions.FileTypeSniffer.Extra.DependencyInjection/Microsoft/Extensions/DependencyInjection/DependencyExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Cosmos.Dependency;
+using Cosmos.Sniffers.FileTypeSniffers;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DependencyExtensions
+    {
+        public static IServiceCollection AddCosmosFileTypeSniffer(this IServiceCollection services, Action<FileTypeSnifferOptions> configure = null)
+        {
+            using (var register = new MicrosoftProxyRegister(services))
+            {
+                register.AddFileTypeSniffer(configure);
+            }
+
+            return services;
+        }
+    }
+}
